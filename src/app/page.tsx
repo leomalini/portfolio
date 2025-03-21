d64b6fe1d4e95e5e87074d03b022c436637667aa
Leonardo Malini Coradini <leomalini61@gmail.com>
2025-03-21 13:33:30 -0300

    add Vercel Analytics for improved tracking and update metadata for SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,7 @@ import { About } from "@/components/sections/about";
 import { Experiences } from "@/components/sections/experiences";
 import { Technologies } from "@/components/sections/tech";
 import { Projects } from "@/components/sections/projects";
-import type { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Leonardo Malini Coradini",
-  description: "...",
-  keywords: [
-    "Web",
-    "FrontEnd Portfolio",
-    "Malini",
-    "Coradini",
-    "React Portfolio",
-    "Front Porfolio",
-    "FrontEnd Developer",
-    "FrontEnd Developer Portfolio",
-  ],
-};
 export default function Home() {
   return (
     <Fragment>
